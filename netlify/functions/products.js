dbdcbd13be3fb435ec0ad2a35c1bf0753869d34c
RewearSA <hello@rewearsa.co.za>
2025-09-13 16:25:44 +0200

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,1 +1,19 @@
+const fetch = require("node-fetch");
+
+exports.handler = async () => {
+  try {
+    const res = await fetch(`https://api.airtable.com/v0/${process.env.baseId}/Products`, {
+      headers: { Authorization: `Bearer ${process.env.token}` }
+    });
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.records)
+    };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+  }
+};
 
